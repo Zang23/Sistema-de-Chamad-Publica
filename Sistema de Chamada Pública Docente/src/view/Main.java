159e84d5046b407d22a340fb152ba2d8bc3c397a
Mimidimas <emilydimas14@gmail.com>
2025-11-12 00:23:50 -0300

    testando

--- Sistema de Chamada Pública Docente/src/view/Main.java
+++ Sistema de Chamada Pública Docente/src/view/Main.java
@@ -11,10 +11,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		CursoArquivo arquivo = new CursoArquivo();
-		Curso curso = new Curso("189", "RH", "humanas");
+		Curso curso2 = new Curso("185", "RH", "Curso_chato");
 		
 		try {
-			arquivo.salvar(curso);
+			arquivo.salvar(curso2);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
