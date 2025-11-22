958c224525465db941cc22c0bb5e6c1f560370c9
Mimidimas <emilydimas14@gmail.com>
2025-11-22 01:23:56 -0300

    @mimidimas - Aumentando o tamanho da tela e ajustando os componentes

--- Sistema de Chamada Pública Docente/src/view/Tela.java
+++ Sistema de Chamada Pública Docente/src/view/Tela.java
@@ -31,13 +31,16 @@ public class Tela extends JFrame {
 	private JTextField txtDiciplinaNome;
 	private JTextField txtDiciplinaCodigo;
 	private JTextField txtDiciplinaHorario;
-	private JTextField txtCodigoCurso;
-	private JTextField txtCodigoProcesso;
+	private JTextField txtDiciplinaCodigoCurso;
+	private JTextField txtDiciplinaCodigoProcesso;
 	private JTextField txtProfessorCPF;
 	private JTextField txtProfessorNome;
 	private JTextField txtProfessorQntdPontos;
 	private JTextField txtCursoCodigo;
 	private JTextField txtCursoNome;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -52,16 +55,15 @@ public class Tela extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	// Criação das telas
+	
 	public Tela() {
 		
-		
+		//Informações gerais da tela
 		setForeground(Color.BLACK);
 		setTitle("Sistema de chamada pública");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 623, 390);
+		setBounds(100, 100, 900, 500);
 		contentPane = new JPanel();
 		contentPane.setForeground(new Color(34, 40, 49));
 		contentPane.setBackground(new Color(34, 40, 49));
@@ -74,113 +76,116 @@ public class Tela extends JFrame {
 		tabbedPane.setBackground(new Color(49, 54, 63));
 		contentPane.add(tabbedPane);
 		
+		
+		// Aba Diciplina
 		JPanel tabDiciplina = new JPanel();
 		tabDiciplina.setBorder(null);
 		tabDiciplina.setForeground(new Color(0, 0, 0));
 		tabDiciplina.setBackground(new Color(49, 54, 63));
 		tabbedPane.addTab("Diciplina", null, tabDiciplina, "Cadastro de diciplina");
+		tabbedPane.setForegroundAt(0, new Color(0, 0, 0));
 		tabbedPane.setBackgroundAt(0, new Color(128, 128, 128));
 		tabDiciplina.setLayout(null);
 		
 		JLabel lblDiciplinaNome = new JLabel("Nome ");
-		lblDiciplinaNome.setBounds(35, 33, 49, 34);
+		lblDiciplinaNome.setBounds(73, 84, 49, 34);
 		lblDiciplinaNome.setBackground(new Color(0, 0, 0));
 		lblDiciplinaNome.setForeground(new Color(255, 255, 255));
 		lblDiciplinaNome.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(lblDiciplinaNome);
 		
 		JLabel lblDicilpinaCodigoDiciplina = new JLabel("Codigo da diciplina");
-		lblDicilpinaCodigoDiciplina.setBounds(35, 5, 112, 34);
+		lblDicilpinaCodigoDiciplina.setBounds(73, 50, 112, 34);
 		lblDicilpinaCodigoDiciplina.setForeground(new Color(255, 255, 255));
 		lblDicilpinaCodigoDiciplina.setBackground(new Color(255, 255, 255));
 		lblDicilpinaCodigoDiciplina.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(lblDicilpinaCodigoDiciplina);
 		
 		JLabel lblDiciplinaDia = new JLabel("Dia da diciplina");
-		lblDiciplinaDia.setBounds(35, 65, 98, 34);
+		lblDiciplinaDia.setBounds(73, 118, 98, 34);
 		lblDiciplinaDia.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblDiciplinaDia.setForeground(new Color(255, 255, 255));
 		lblDiciplinaDia.setBackground(new Color(255, 255, 255));
 		tabDiciplina.add(lblDiciplinaDia);
 		
 		JLabel lblDiciplinaHorario = new JLabel("Horario");
-		lblDiciplinaHorario.setBounds(35, 95, 61, 34);
+		lblDiciplinaHorario.setBounds(73, 154, 61, 34);
 		lblDiciplinaHorario.setForeground(new Color(255, 255, 255));
 		lblDiciplinaHorario.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(lblDiciplinaHorario);
 		
 		JLabel lblDiciplinaQuantAula = new JLabel("Aula diária");
-		lblDiciplinaQuantAula.setBounds(321, 11, 77, 23);
+		lblDiciplinaQuantAula.setBounds(507, 56, 77, 23);
 		lblDiciplinaQuantAula.setForeground(new Color(255, 255, 255));
 		lblDiciplinaQuantAula.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(lblDiciplinaQuantAula);
 		
 		JLabel lblDiciplinaCodCurso = new JLabel("Código do curso");
-		lblDiciplinaCodCurso.setBounds(321, 39, 112, 23);
+		lblDiciplinaCodCurso.setBounds(507, 90, 112, 23);
 		lblDiciplinaCodCurso.setForeground(new Color(255, 255, 255));
 		lblDiciplinaCodCurso.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(lblDiciplinaCodCurso);
 		
 		JLabel lblDiciplinaCodProcesso = new JLabel("Código do processo");
-		lblDiciplinaCodProcesso.setBounds(321, 71, 128, 23);
+		lblDiciplinaCodProcesso.setBounds(507, 124, 128, 23);
 		lblDiciplinaCodProcesso.setForeground(new Color(255, 255, 255));
 		lblDiciplinaCodProcesso.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(lblDiciplinaCodProcesso);
 		
 		txtDiciplinaNome = new JTextField();
-		txtDiciplinaNome.setBounds(107, 41, 180, 20);
+		txtDiciplinaNome.setBounds(132, 91, 180, 20);
 		txtDiciplinaNome.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(txtDiciplinaNome);
 		txtDiciplinaNome.setColumns(10);
 		
 		txtDiciplinaCodigo = new JTextField();
-		txtDiciplinaCodigo.setBounds(157, 12, 61, 20);
+		txtDiciplinaCodigo.setBounds(251, 57, 61, 20);
 		txtDiciplinaCodigo.setFont(new Font("Arial", Font.PLAIN, 12));
 		txtDiciplinaCodigo.setColumns(10);
 		tabDiciplina.add(txtDiciplinaCodigo);
 		
 		txtDiciplinaHorario = new JTextField();
-		txtDiciplinaHorario.setBounds(183, 103, 101, 20);
+		txtDiciplinaHorario.setBounds(211, 125, 101, 20);
 		txtDiciplinaHorario.setFont(new Font("Arial", Font.PLAIN, 12));
 		txtDiciplinaHorario.setColumns(10);
 		tabDiciplina.add(txtDiciplinaHorario);
 		
-		txtCodigoCurso = new JTextField();
-		txtCodigoCurso.setBounds(461, 41, 77, 20);
-		txtCodigoCurso.setFont(new Font("Arial", Font.PLAIN, 12));
-		txtCodigoCurso.setColumns(10);
-		tabDiciplina.add(txtCodigoCurso);
+		txtDiciplinaCodigoCurso = new JTextField();
+		txtDiciplinaCodigoCurso.setBounds(665, 91, 77, 20);
+		txtDiciplinaCodigoCurso.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtDiciplinaCodigoCurso.setColumns(10);
+		tabDiciplina.add(txtDiciplinaCodigoCurso);
 		
-		txtCodigoProcesso = new JTextField();
-		txtCodigoProcesso.setBounds(461, 73, 77, 20);
-		txtCodigoProcesso.setFont(new Font("Arial", Font.PLAIN, 12));
-		txtCodigoProcesso.setColumns(10);
-		tabDiciplina.add(txtCodigoProcesso);
+		txtDiciplinaCodigoProcesso = new JTextField();
+		txtDiciplinaCodigoProcesso.setBounds(665, 125, 77, 20);
+		txtDiciplinaCodigoProcesso.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtDiciplinaCodigoProcesso.setColumns(10);
+		tabDiciplina.add(txtDiciplinaCodigoProcesso);
 		
 		JComboBox comboBoxDiciplinaAulaDiaria = new JComboBox();
-		comboBoxDiciplinaAulaDiaria.setBounds(461, 12, 77, 22);
+		comboBoxDiciplinaAulaDiaria.setBounds(665, 56, 77, 22);
 		comboBoxDiciplinaAulaDiaria.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(comboBoxDiciplinaAulaDiaria);
 		
 		JComboBox comboBoxDiciplinaDia = new JComboBox();
-		comboBoxDiciplinaDia.setBounds(183, 72, 101, 22);
+		comboBoxDiciplinaDia.setBounds(211, 160, 101, 22);
 		comboBoxDiciplinaDia.setFont(new Font("Arial", Font.PLAIN, 12));
 		tabDiciplina.add(comboBoxDiciplinaDia);
 		
 		JButton btnDiciplinaBuscar = new JButton("🔎");
-		btnDiciplinaBuscar.setBounds(228, 11, 49, 23);
+		btnDiciplinaBuscar.setBounds(774, 194, 49, 24);
 		tabDiciplina.add(btnDiciplinaBuscar);
 		
 		JScrollPane scrollPaneDiciplina = new JScrollPane();
-		scrollPaneDiciplina.setBounds(35, 137, 513, 153);
+		scrollPaneDiciplina.setBounds(39, 234, 783, 159);
 		tabDiciplina.add(scrollPaneDiciplina);
 		
 		JButton btnDiciplinaCadastrar = new JButton("Cadastrar");
-		btnDiciplinaCadastrar.setBounds(426, 105, 112, 24);
+		btnDiciplinaCadastrar.setBounds(640, 194, 112, 24);
 		tabDiciplina.add(btnDiciplinaCadastrar);
 		
 		JButton btnDiciplinaLimparTela = new JButton("Limpar tela");
-		btnDiciplinaLimparTela.setBounds(304, 105, 112, 24);
+		btnDiciplinaLimparTela.setBounds(507, 194, 112, 24);
 		tabDiciplina.add(btnDiciplinaLimparTela);
 		
 		JPanel tabProfessor = new JPanel();
@@ -194,66 +199,66 @@ public class Tela extends JFrame {
 		lblProfessorCPF.setForeground(Color.WHITE);
 		lblProfessorCPF.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblProfessorCPF.setBackground(Color.WHITE);
-		lblProfessorCPF.setBounds(35, 5, 112, 34);
+		lblProfessorCPF.setBounds(73, 50, 112, 34);
 		tabProfessor.add(lblProfessorCPF);
 		
 		txtProfessorCPF = new JTextField();
 		txtProfessorCPF.setFont(new Font("Arial", Font.PLAIN, 12));
 		txtProfessorCPF.setColumns(10);
-		txtProfessorCPF.setBounds(106, 12, 112, 20);
+		txtProfessorCPF.setBounds(203, 57, 112, 20);
 		tabProfessor.add(txtProfessorCPF);
 		
 		JLabel lblProfessorNome = new JLabel("Nome");
 		lblProfessorNome.setForeground(Color.WHITE);
 		lblProfessorNome.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblProfessorNome.setBackground(Color.WHITE);
-		lblProfessorNome.setBounds(35, 37, 112, 34);
+		lblProfessorNome.setBounds(73, 97, 49, 34);
 		tabProfessor.add(lblProfessorNome);
 		
 		txtProfessorNome = new JTextField();
 		txtProfessorNome.setFont(new Font("Arial", Font.PLAIN, 12));
 		txtProfessorNome.setColumns(10);
-		txtProfessorNome.setBounds(106, 44, 171, 20);
+		txtProfessorNome.setBounds(144, 104, 171, 20);
 		tabProfessor.add(txtProfessorNome);
 		
 		JButton btnProfessorBuscar = new JButton("🔎");
-		btnProfessorBuscar.setBounds(228, 11, 49, 23);
+		btnProfessorBuscar.setBounds(774, 194, 49, 24);
 		tabProfessor.add(btnProfessorBuscar);
 		
 		JLabel lblProfessorArea = new JLabel("Area");
 		lblProfessorArea.setForeground(Color.WHITE);
 		lblProfessorArea.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblProfessorArea.setBackground(Color.WHITE);
-		lblProfessorArea.setBounds(320, 5, 112, 34);
+		lblProfessorArea.setBounds(507, 56, 77, 23);
 		tabProfessor.add(lblProfessorArea);
 		
 		JLabel lblProfessorQntdPontos = new JLabel("Quantidade de pontos");
 		lblProfessorQntdPontos.setForeground(Color.WHITE);
 		lblProfessorQntdPontos.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblProfessorQntdPontos.setBackground(Color.WHITE);
-		lblProfessorQntdPontos.setBounds(320, 37, 181, 34);
+		lblProfessorQntdPontos.setBounds(507, 97, 181, 34);
 		tabProfessor.add(lblProfessorQntdPontos);
 		
 		txtProfessorQntdPontos = new JTextField();
 		txtProfessorQntdPontos.setFont(new Font("Arial", Font.PLAIN, 12));
 		txtProfessorQntdPontos.setColumns(10);
-		txtProfessorQntdPontos.setBounds(454, 44, 61, 20);
+		txtProfessorQntdPontos.setBounds(656, 104, 61, 20);
 		tabProfessor.add(txtProfessorQntdPontos);
 		
 		JComboBox comboBoxProfessorArea = new JComboBox();
-		comboBoxProfessorArea.setBounds(402, 11, 113, 22);
+		comboBoxProfessorArea.setBounds(604, 56, 113, 22);
 		tabProfessor.add(comboBoxProfessorArea);
 		
 		JScrollPane scrollPaneProfessor = new JScrollPane();
-		scrollPaneProfessor.setBounds(35, 137, 513, 153);
+		scrollPaneProfessor.setBounds(39, 234, 783, 159);
 		tabProfessor.add(scrollPaneProfessor);
 		
 		JButton btnProfessorLimparTela = new JButton("Limpar tela");
-		btnProfessorLimparTela.setBounds(157, 87, 112, 24);
+		btnProfessorLimparTela.setBounds(507, 194, 112, 24);
 		tabProfessor.add(btnProfessorLimparTela);
 		
 		JButton btnProfessorCadastrar = new JButton("Cadastrar");
-		btnProfessorCadastrar.setBounds(294, 87, 112, 24);
+		btnProfessorCadastrar.setBounds(640, 194, 112, 24);
 		tabProfessor.add(btnProfessorCadastrar);
 		
 		JPanel tabCurso = new JPanel();
@@ -267,53 +272,53 @@ public class Tela extends JFrame {
 		lblCursoCodigo.setForeground(Color.WHITE);
 		lblCursoCodigo.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblCursoCodigo.setBackground(Color.WHITE);
-		lblCursoCodigo.setBounds(35, 5, 112, 34);
+		lblCursoCodigo.setBounds(73, 50, 112, 34);
 		tabCurso.add(lblCursoCodigo);
 		
 		txtCursoCodigo = new JTextField();
 		txtCursoCodigo.setFont(new Font("Arial", Font.PLAIN, 12));
 		txtCursoCodigo.setColumns(10);
-		txtCursoCodigo.setBounds(146, 12, 72, 20);
+		txtCursoCodigo.setBounds(231, 57, 72, 20);
 		tabCurso.add(txtCursoCodigo);
 		
 		JButton btnCursoBuscar = new JButton("🔎");
-		btnCursoBuscar.setBounds(228, 11, 49, 23);
+		btnCursoBuscar.setBounds(774, 194, 49, 24);
 		tabCurso.add(btnCursoBuscar);
 		
 		JLabel lblCursoNome = new JLabel("Nome");
 		lblCursoNome.setForeground(Color.WHITE);
 		lblCursoNome.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblCursoNome.setBackground(Color.WHITE);
-		lblCursoNome.setBounds(35, 37, 112, 34);
+		lblCursoNome.setBounds(73, 97, 49, 34);
 		tabCurso.add(lblCursoNome);
 		
 		txtCursoNome = new JTextField();
 		txtCursoNome.setFont(new Font("Arial", Font.PLAIN, 12));
 		txtCursoNome.setColumns(10);
-		txtCursoNome.setBounds(106, 44, 171, 20);
+		txtCursoNome.setBounds(132, 104, 171, 20);
 		tabCurso.add(txtCursoNome);
 		
 		JLabel lblCursoArea = new JLabel("Area");
 		lblCursoArea.setForeground(Color.WHITE);
 		lblCursoArea.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblCursoArea.setBackground(Color.WHITE);
-		lblCursoArea.setBounds(320, 5, 112, 34);
+		lblCursoArea.setBounds(507, 50, 112, 34);
 		tabCurso.add(lblCursoArea);
 		
 		JComboBox comboBoxCursoArea = new JComboBox();
-		comboBoxCursoArea.setBounds(402, 11, 113, 22);
+		comboBoxCursoArea.setBounds(604, 56, 113, 22);
 		tabCurso.add(comboBoxCursoArea);
 		
 		JButton btnCursoLimparTela = new JButton("Limpar tela");
-		btnCursoLimparTela.setBounds(157, 87, 112, 24);
+		btnCursoLimparTela.setBounds(507, 194, 112, 24);
 		tabCurso.add(btnCursoLimparTela);
 		
 		JButton btnCursoCadastrar = new JButton("Cadastrar");
-		btnCursoCadastrar.setBounds(294, 87, 112, 24);
+		btnCursoCadastrar.setBounds(640, 194, 112, 24);
 		tabCurso.add(btnCursoCadastrar);
 		
 		JScrollPane scrollPaneCurso = new JScrollPane();
-		scrollPaneCurso.setBounds(35, 137, 513, 153);
+		scrollPaneCurso.setBounds(39, 234, 783, 159);
 		tabCurso.add(scrollPaneCurso);
 		
 		JPanel tabInscricao = new JPanel();
@@ -326,22 +331,56 @@ public class Tela extends JFrame {
 		lblInscricaoCpfProfessor.setForeground(Color.WHITE);
 		lblInscricaoCpfProfessor.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblInscricaoCpfProfessor.setBackground(Color.WHITE);
-		lblInscricaoCpfProfessor.setBounds(35, 5, 112, 34);
+		lblInscricaoCpfProfessor.setBounds(73, 50, 112, 34);
 		tabInscricao.add(lblInscricaoCpfProfessor);
 		
 		JLabel lblInscricaoCodDiciplina = new JLabel("Código da diciplina");
 		lblInscricaoCodDiciplina.setForeground(Color.WHITE);
 		lblInscricaoCodDiciplina.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblInscricaoCodDiciplina.setBackground(Color.WHITE);
-		lblInscricaoCodDiciplina.setBounds(35, 36, 112, 34);
+		lblInscricaoCodDiciplina.setBounds(73, 97, 137, 34);
 		tabInscricao.add(lblInscricaoCodDiciplina);
 		
 		JLabel lblInscricaoCodProcesso = new JLabel("Código do processo");
 		lblInscricaoCodProcesso.setForeground(Color.WHITE);
 		lblInscricaoCodProcesso.setFont(new Font("Arial", Font.PLAIN, 12));
 		lblInscricaoCodProcesso.setBackground(Color.WHITE);
-		lblInscricaoCodProcesso.setBounds(324, 5, 112, 34);
+		lblInscricaoCodProcesso.setBounds(460, 50, 112, 34);
 		tabInscricao.add(lblInscricaoCodProcesso);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Arial", Font.PLAIN, 12));
+		textField.setColumns(10);
+		textField.setBounds(227, 57, 72, 20);
+		tabInscricao.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		textField_1.setColumns(10);
+		textField_1.setBounds(227, 104, 72, 20);
+		tabInscricao.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		textField_2.setColumns(10);
+		textField_2.setBounds(610, 57, 72, 20);
+		tabInscricao.add(textField_2);
+		
+		JButton btnCursoLimparTela_1 = new JButton("Limpar tela");
+		btnCursoLimparTela_1.setBounds(507, 194, 112, 24);
+		tabInscricao.add(btnCursoLimparTela_1);
+		
+		JButton btnCursoCadastrar_1 = new JButton("Cadastrar");
+		btnCursoCadastrar_1.setBounds(640, 194, 112, 24);
+		tabInscricao.add(btnCursoCadastrar_1);
+		
+		JButton btnCursoBuscar_1 = new JButton("🔎");
+		btnCursoBuscar_1.setBounds(774, 194, 49, 24);
+		tabInscricao.add(btnCursoBuscar_1);
+		
+		JScrollPane scrollPaneCurso_1 = new JScrollPane();
+		scrollPaneCurso_1.setBounds(39, 234, 783, 159);
+		tabInscricao.add(scrollPaneCurso_1);
 
 	}
 }
